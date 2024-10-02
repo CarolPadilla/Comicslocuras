b716374133a4f67ce93a43871b0ea15afa635b09
HectorCor <163451390+HectorCor@users.noreply.github.com>
2024-10-02 18:20:24 -0300

    Fear

--- Comicslocuras/src/app/home/home.page.ts
+++ Comicslocuras/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private sqlite: SQLite) {}
 
 }
---
+++ Comicslocuras/src/app/services/crud.ts
@@ -0,0 +1,8 @@
+export class Crud {
+    id_producto!: number;
+    precio_p!: number;
+    nombre_p!: string;
+    stock!: number;
+    descripcion!: string;
+    foto_p!: string;
+}
