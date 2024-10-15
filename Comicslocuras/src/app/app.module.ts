49b26a7d9de6c9390653581e25e6e7e59fdab7f5
HectorCor <163451390+HectorCor@users.noreply.github.com>
2024-10-15 14:00:18 -0300

    api flashlight quitada

--- Comicslocuras/src/app/app.module.ts
+++ Comicslocuras/src/app/app.module.ts
@@ -11,12 +11,11 @@ import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { HttpClientModule } from '@angular/common/http';
 
-import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativeStorage, SQLite, Flashlight],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativeStorage, SQLite,],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
