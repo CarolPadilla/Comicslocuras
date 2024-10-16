3b51fc255a179b67a992e5eacd60a93552ec0dd3
HectorCor <163451390+HectorCor@users.noreply.github.com>
2024-10-16 15:39:42 -0300

    Detalle accesorios

--- Comicslocuras/src/app/pages/accesorios/accesorios.page.ts
+++ Comicslocuras/src/app/pages/accesorios/accesorios.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-accesorios',
@@ -55,17 +56,26 @@ productos = [
     // Añadir más productos aquí
   ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() { }
 
   // Función para manejar la compra de un producto
-  comprarProducto(id: number) {
+  vermasProducto(producto: any) {
+    //redirigir los datos del producto a un html interior
+    let navigationsextras: NavigationExtras = {
+      state: {
+        prod: producto
+      }
+    }
+    this.router.navigate(['/detalleproducto'], navigationsextras);
+
+    /*
     const productoSeleccionado = this.productos.find(producto => producto.id === id);
     if (productoSeleccionado) {
       console.log('Producto seleccionado:', productoSeleccionado);
       // Aquí puedes agregar la lógica de compra o navegación
-    }
+    }*/
   }
 }
 
